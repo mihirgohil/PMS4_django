419d5660a4d5c57a7e4595b653be6a70b7013f9d
mihirgohil <mihirgohil9725@gmail.com>
2020-10-24 16:26:05 +0530

    models updated

--- PMS4/placement_management/models.py
+++ PMS4/placement_management/models.py
@@ -162,11 +162,9 @@ def create_user_profile(sender, instance, created, **kwargs):
         if instance.user_type == 1:
             AdminPms.objects.create(admin=instance)
         if instance.user_type == 2:
-            Companys.objects.create(admin=instance, address="")
+            Companys.objects.create(admin=instance)
         if instance.user_type == 3:
-            Students.objects.create(admin=instance, course_id=Students.objects.get(id=1),
-                                    session_start_year="2020-01-01", session_end_year="2021-01-01", address="",
-                                    profile_pic="", gender="")
+            Students.objects.create(admin=instance)
 
 
 # save user profile
